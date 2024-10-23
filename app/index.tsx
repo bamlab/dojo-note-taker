6d9afe5a428034ed832d3385a5691c0ef44b3846
Mikhail Dyatlov <mdyatlov@protonmail.com>
2024-10-23 14:57:01 +0200

    feat(app): added a loader and updateed the design

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
 import { Audio } from "expo-av";
 import axios from "axios";
 
@@ -7,7 +9,7 @@ const OPENAI_API_KEY = "";
 
 export default function HomeScreen() {
   const [recording, setRecording] = useState<Audio.Recording>();
-  const [summary, setSummary] = useState("");
+  const [summary, setSummary] = useState();
 
   const startRecording = async () => {
     try {
@@ -30,6 +32,7 @@ export default function HomeScreen() {
 
   const stopRecording = async () => {
     if (recording) {
+      setSummary(undefined);
       await recording.stopAndUnloadAsync();
       await Audio.setAudioModeAsync(
         {
@@ -38,7 +41,7 @@ export default function HomeScreen() {
       );
       const uri = recording.getURI();
       console.log("Recording stopped and stored at", uri);
-      
+
       if (uri === null) return;
 
       console.log("Sending the recording to the Whisper API...\n");
@@ -58,13 +61,61 @@ export default function HomeScreen() {
   return (
     <View
       style={styles.container}>
-      <Button
-        title={recording ? "Stop Recording" : "Start Recording"}
-        onPress={recording ? stopRecording : startRecording}
-      />
-      <Text>
-        {summary}
-      </Text>
+
+      <View style={{
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        padding: 20,
+      }}>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            borderRadius: 12,
+            marginBottom: 20,
+            padding: 20,
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#283833", fontSize: 16, }}>
+            {summary ? summary : "Start the recording to take a note. The summary of the note will appear after you stop the recording."}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={recording ? stopRecording : startRecording}
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            height: 64,
+            borderRadius: 12,
+            backgroundColor: "#205735",
+            justifyContent: "center",
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.2,
+            shadowRadius: 10,
+            elevation: 5, // For Android shadow
+          }}
+        >
+
+          {recording && !summary && (
+            <View style={{ marginRight: 8 }}>
+              <ActivityIndicator size="small" color="#ffffff" />
+            </View>
+          )}
+          {(!recording || summary) && <MaterialIcons name={recording ? "stop" : "mic"} size={24} color="white" />}
+          {(!recording || summary) && <Text style={{ color: "white", fontSize: 20, marginLeft: 8 }}>
+            {recording ? "Stop and summarize" : "New Recording"}
+          </Text>}
+        </TouchableOpacity>
+
+      </View>
+
+
+
+
     </View>
   );
 }
@@ -107,7 +158,7 @@ const summarizeText = async (text: string) => {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
-        messages: [{"role": "user", "content": `Summarize the following transcript:\n\n${text}`}],
+        messages: [{ "role": "user", "content": `Summarize the following transcript:\n\n${text}` }],
         model: "gpt-4o-mini",
         temperature: 0.7,
       },
@@ -125,3 +176,5 @@ const summarizeText = async (text: string) => {
     return null;
   }
 };
+
+// 7bb665
